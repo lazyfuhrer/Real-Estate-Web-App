0b17c59b855f1251591f0ff4494613d499685cd4
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-10-09 15:41:41 -0400

    fix up navbar ui

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,22 +5,20 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => (
-    <>
-        <Head>
-            <title>Rent & Buy Properties</title>
-        </Head>
-        <Box maxWidth="1280px" m="auto">
-            <header>
-                <Navbar/>
-            </header>
-            <main>
-                {children}
-            </main>
-            <footer>
-                <Footer/>
-            </footer>
-        </Box>
-    </>
+	<>
+		<Head>
+			<title>Rent & Buy Properties</title>
+		</Head>
+		<header>
+			<Navbar />
+		</header>
+		<Box maxWidth="1280px" m="auto">
+			<main>{children}</main>
+		</Box>
+		<footer>
+			<Footer />
+		</footer>
+	</>
 );
 
 export default Layout;

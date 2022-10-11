97d60cad78aab5782c843334f06438bfbeb6a19b
Aryan <singh.aryan837@gmail.com>
2022-10-11 14:16:18 -0400

    Responsive footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,24 +3,22 @@ import NewLogo from "../assets/images/newLogoNoBg.png";
 import { FaLinkedin,FaGithub } from 'react-icons/fa';
 
 const Footer = () => (
-  // <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
-  //     © 2022 Magic Properties, Inc.
-  //     <Box color="green.600">Made with ❤ by&nbsp;
-  //         <a href="https://www.linkedin.com/in/biswarghya-biswas/">Biswarghya Biswas</a>
-  //     </Box>
-  // </Box>
+
+  <>
   <Stack p='5' borderTop='1px' borderColor='gray.100' direction={['column','row']} justify='space-around' alignItems={['flex-start','center']}>
    
     <Flex  maxWidth='250px' direction='column' >
       <Image boxSize="100px" src={NewLogo.src} alt="logo" position='relative'/>
       <Text align={['start','center']} >Magic Properties is an Online platform to Buy or Rent top Properties in Dubai. It caters to a global market with its unique services and novel online features.</Text>
     </Flex>
+    {/* Company related fields */}
     <Flex direction='column'>
       <Text fontWeight='semibold' fontSize='xl' mb='5px'>Company</Text>
       <Link href="#">Privacy Policy</Link>
       <Link href="#">Terms and Conditions</Link>
       <Link href="#">Contributers</Link>
     </Flex>
+    {/* Useful links fields */}
     <Flex direction='column'>
       <Text fontWeight='semibold' fontSize='xl' mb='5px'>Useful Links</Text>
           <Link href="/search?purpose=for-sale" passHref>
@@ -33,6 +31,7 @@ const Footer = () => (
             About Us
           </Link>
     </Flex>
+    {/* Contact up fields */}
     <Flex direction='column'>
       <Text fontWeight='semibold' fontSize='xl' mb='5px'>Contact us</Text>
       <Link
@@ -50,6 +49,16 @@ const Footer = () => (
       <Link href="https://github.com/lazyfuhrer/Real-Estate-Web-App">Repository</Link>
     </Flex>    
   </Stack>
+  <Box textAlign="center" p="5" color="gray.600">
+      © 2022 Magic Properties, Inc.
+      <Box color="green.600">Made with ❤ by&nbsp;
+          <a href="https://www.linkedin.com/in/biswarghya-biswas/">Biswarghya Biswas</a>
+      </Box>
+  </Box>
+
+  </>
+
+  
 )
 
 export default Footer;

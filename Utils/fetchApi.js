1ed710ac3026f624fcb18e8ab8acf3a5e51631c0
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2022-10-12 21:23:01 -0700

    improved search filter UI
    Rearranged search by location components

--- Utils/fetchApi.js
+++ Utils/fetchApi.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export const baseUrl = 'https://bayut.p.rapidapi.com'
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async (url) => {
-    const { data } = await axios.get((url), {
-        headers: {
-            'X-RapidAPI-Key': '7507ea973fmshe291fcc7e43e58dp16a271jsn79f6aa8cc871',
-            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
-        }
-    });
-    
-    return data;
-}
+  const { data } = await axios.get(url, {
+    headers: {
+      "X-RapidAPI-Key": "15e24392e6msh8273085aca8cfe5p1bc39ajsn44b963901f78",
+      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+    },
+  });
+
+  return data;
+};
--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -5,22 +5,21 @@ import {
   Box,
   Text,
   Input,
-  Spinner,
   Icon,
   Button,
   useColorModeValue,
-  Spacer,
   InputGroup,
   InputRightAddon,
+  CircularProgress,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { CloseIcon } from "@chakra-ui/icons";
-// import { MdCancel } from "react-icons/md";
-import Image from "next/image";
+import AsyncSelect from "react-select/async";
+// import Image from "next/image";
 
 import { filterData, getFilterValues } from "../Utils/filterData";
 import { baseUrl, fetchApi } from "../Utils/fetchApi";
-import noresult from "../assets/images/noresult.svg";
+// import noresult from "../assets/images/noresult.svg";
 
 export default function SearchFilters() {
   const [filters] = useState(filterData);
@@ -32,9 +31,9 @@ export default function SearchFilters() {
 
   // color modes for light/ dark
   const searchbg = useColorModeValue("gray.100", "gray.700");
-
+  const inputcolor = useColorModeValue("gray.800", "gray.200");
   const searchProperties = (filterValues) => {
-    setLoading(true);
+    setLoading((state) => true);
     const path = router.pathname;
     const { query } = router;
 
@@ -47,9 +46,29 @@ export default function SearchFilters() {
     });
 
     router.push({ pathname: path, query: query });
-    setLoading(false);
+    setLoading((state) => false);
   };
-
+  // const promiseOptions = async (inputValue) => {
+  //   setLoading(() => true);
+  //   const data = await fetchApi(`${baseUrl}/auto-complete?query=${inputValue}`);
+  //   setLoading(() => false);
+  //   setLocationData(data?.hits);
+  //   console.log(data.hits);
+  //   return data?.hits;
+  // };
+  // const customStyles = {
+  //   container: (provided, state) => ({
+  //     ...provided,
+  //     width: 500,
+  //     cursor: "pointer",
+  //     backgroundColor: "transperant",
+  //   }),
+  //   menu: (provided, state) => ({
+  //     ...provided,
+  //     color: useColorModeValue("white", "black"),
+  //     backgroundColor: useColorModeValue("gray", "white"),
+  //   }),
+  // };
   useEffect(() => {
     if (searchTerm !== "") {
       const fetchData = async () => {
@@ -66,8 +85,8 @@ export default function SearchFilters() {
   }, [searchTerm]);
 
   return (
-    <Flex bg={searchbg} p="4" justifyContent="center" flexWrap="wrap">
-      <Flex wrap="wrap" justifyContent="center">
+    <Flex bg={searchbg} justifyContent="center" flexWrap="wrap">
+      <Flex wrap="wrap" justifyContent="center" p="4">
         {filters?.map((filter) => (
           <Box key={filter.queryName}>
             <Select
@@ -86,11 +105,29 @@ export default function SearchFilters() {
           </Box>
         ))}
       </Flex>
-      <Spacer />
-      <Flex>
+      {/* 
+      <Flex marginY={5} justifyContent="center" width="100%">
+        <AsyncSelect
+          styles={customStyles}
+          cacheOptions
+          loadOptions={promiseOptions}
+          getOptionLabel={(option) => `${option.name}`}
+        />
+      </Flex> */}
+
+      <Flex
+        width="100%"
+        borderTop="0.5px lightgray solid"
+        justifyContent="center"
+        wrap="wrap"
+        paddingY={5}>
         {showLocations && (
-          <Flex pos="relative" paddingTop="2">
-            <InputGroup focusBorderColor="gray.300" w="300px">
+          <Flex>
+            <InputGroup
+              focusBorderColor="gray.300"
+              w="300px"
+              margin={3}
+              minWidth={"30%"}>
               <Input
                 placeholder="Type Here"
                 value={searchTerm}
@@ -107,20 +144,32 @@ export default function SearchFilters() {
                   />
                 </InputRightAddon>
               )}
-              {loading && <Spinner margin="auto" marginTop="3" />}
+              {loading && (
+                <CircularProgress isIndeterminate marginX={5} height={1} />
+              )}
             </InputGroup>
-            {showLocations && (
-              <Select overflow="auto">
+            {showLocations && !loading && locationData?.length > 0 && (
+              <Select
+                minWidth={"30%"}
+                overflow="auto"
+                margin={3}
+                value={searchTerm}
+                onChange={(e) => {
+                  const location = JSON.parse(e.target.value);
+                  console.log(location);
+                  searchProperties({
+                    locationExternalIDs: location.externalID,
+                  });
+                  // setShowLocations(false);
+                  setSearchTerm((state) => location.name);
+                }}>
                 {locationData?.map((location) => (
                   <option
                     key={location.id}
-                    onClick={() => {
-                      searchProperties({
-                        locationExternalIDs: location.externalID,
-                      });
-                      setShowLocations(false);
-                      setSearchTerm(location.name);
-                    }}>
+                    value={JSON.stringify({
+                      name: location.name,
+                      externalID: location.externalID,
+                    })}>
                     {/* <Text
                       cursor="pointer"
                       bg="gray.200"
@@ -152,8 +201,8 @@ export default function SearchFilters() {
           onClick={() => setShowLocations(!showLocations)}
           border="1px"
           borderColor="gray.200"
-          marginTop="2">
-          Search Location
+          margin={3}>
+          Search By Location
         </Button>
       </Flex>
     </Flex>

f660ffe7aa3b117fb17f3ae3da54af502993ecff
Biswarghya Biswas <biswasbiswarghya@gmail.com>
2022-10-14 12:17:56 +0530

    Update fetchApi.js

--- Utils/fetchApi.js
+++ Utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
 
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': '15e24392e6msh8273085aca8cfe5p1bc39ajsn44b963901f78',
+            'X-RapidAPI-Key': '579036b614msh8e9e6140aedf3d2p1b09dejsn4190c4a303de',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     });

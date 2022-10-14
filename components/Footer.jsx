8aabe15246788ca75e987e4506fbd4a0b0139e4d
anonimak <sanonimak@gmail.com>
2022-10-14 15:48:38 +0700

    feat:

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
-import NewLogo from "../assets/images/newLogoNoBg.png";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import NewLogo from '../assets/images/newLogoNoBg.png';
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
 
 import {
   Box,
@@ -11,27 +11,27 @@ import {
   Stack,
   Text,
   useColorModeValue,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
 const ListHeader = ({ children }) => {
   return (
-    <Text fontWeight={"500"} fontSize={"xl"} mb={2}>
+    <Text fontWeight={'500'} fontSize={'xl'} mb={2}>
       {children}
     </Text>
   );
 };
 
 const Footer = () => (
-  <Box borderTop={"1px"} borderColor="gray.100">
-    <Container as={Stack} maxW={"6xl"} py={10}>
+  <Box borderTop={'1px'} borderColor="gray.100">
+    <Container as={Stack} maxW={'6xl'} py={10}>
       <SimpleGrid columns={{ base: 1, sm: 2, md: 2 }} spacing={8}>
-        <Stack align={"center"}>
+        <Stack align={'center'}>
           <ListHeader>Company</ListHeader>
           <Link href="#">Privacy Policy</Link>
           <Link href="#">Terms and Conditions</Link>
           <Link href="#">Contributers</Link>
         </Stack>
-        <Stack align={"center"}>
+        <Stack align={'center'}>
           <ListHeader>Useful Links</ListHeader>
           <Link href="/search?purpose=for-sale" passHref>
             Buy Property
@@ -47,18 +47,18 @@ const Footer = () => (
     </Container>
     <Box pb={10}>
       <Flex
-        align={"center"}
+        align={'center'}
         _before={{
           content: '""',
-          borderBottom: "1px solid",
-          borderColor: useColorModeValue("gray.200", "gray.700"),
+          borderBottom: '1px solid',
+          borderColor: useColorModeValue('gray.200', 'gray.700'),
           flexGrow: 1,
           mr: 8,
         }}
         _after={{
           content: '""',
-          borderBottom: "1px solid",
-          borderColor: useColorModeValue("gray.200", "gray.700"),
+          borderBottom: '1px solid',
+          borderColor: useColorModeValue('gray.200', 'gray.700'),
           flexGrow: 1,
           ml: 8,
         }}
@@ -70,21 +70,24 @@ const Footer = () => (
           position="relative"
         />
       </Flex>
-      <Text pt={6} fontSize={"sm"} textAlign={"center"} color="gray.600">
+      <Text pt={6} fontSize={'sm'} textAlign={'center'} color="gray.600">
         © 2022 Magic Properties, Inc. All rights reserved
       </Text>
-      <Text fontSize={"sm"} textAlign={"center"} color="green.600">
+      <Text fontSize={'sm'} textAlign={'center'} color="green.600">
         Made with ❤ by &nbsp;
         <a href="https://www.linkedin.com/in/biswarghya-biswas/">
           Biswarghya Biswas
         </a>
       </Text>
-      <Stack direction={"row"} spacing={6} justifyContent={"center"} mt={6}>
+      <Stack direction={'row'} spacing={6} justifyContent={'center'} mt={6}>
         <Link href="https://www.linkedin.com/in/biswarghya-biswas/">
           <FaLinkedin size={30} />
         </Link>
         <Link href="https://github.com/lazyfuhrer/">
           <FaGithub size={30} />
+        </Link>
+        <Link href="https://twitter.com/BiswarghyaB">
+          <FaTwitter size={30} />
         </Link>
       </Stack>
     </Box>
